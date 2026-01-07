6112448c936ba7a02f789bdc45d92302e6014f94
Sabeeir Sharrma <87631627+SabeeirSharrma@users.noreply.github.com>
2026-01-07 12:52:05 +0530

    upd

--- main.lua
+++ main.lua
@@ -1,75 +1,96 @@
+-- Kavo UI Loader
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("T3rmin4t0r H4x- BY sabplay#7343", "DarkTheme")
+local Window  = Library.CreateLib("T3rmin4t0r H4x- BY sabplay#7343", "DarkTheme")
 
+---------------------------------------------------------------------
+-- Arsenal (PlaceId: 286090429)
+---------------------------------------------------------------------
 if game.PlaceId == 286090429 then
--- [ MAIN ] --
-local M4in = Window:NewTab("M4in")
-local Sh00t3r = M4in:NewSection("4rs3n4l")
+    -- Optional: universal client bypass for Arsenal
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/V.G_Hub_Extras/main/Universal_Client_Bypass"))()
 
-Sh00t3r:NewButton("4iml0ck+4imb0t", "s4fe", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/sabplay/multiscriptgateway/main/arsenal.lua'))()
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "G0 B4NG TH31R M0TH3RS!"; -- the title (ofc)
-        Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-        Icon = "rbxassetid://14682870738"; -- the image if u want. 
-        Duration = 5; -- how long the notification should in secounds
-         })
- end)
+    local M4in    = Window:NewTab("M4in")
+    local Sh00t3r = M4in:NewSection("4rs3n4l")
+
+    Sh00t3r:NewButton("4iml0ck+4imb0t", "s4fe", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/sabplay/multiscriptgateway/main/arsenal.lua"))()
+        game.StarterGui:SetCore("SendNotification", {
+            Title    = "G0 B4NG TH31R M0TH3RS!";
+            Text     = "T3rmin4tor, - Exploiting at it's finest";
+            Icon     = "rbxassetid://14682870738";
+            Duration = 5;
+        })
+    end)
 end
+
+---------------------------------------------------------------------
+-- T3ST Game (PlaceId: 12562525439)
+---------------------------------------------------------------------
 if game.PlaceId == 12562525439 then
     local M4in = Window:NewTab("M4in")
     local T3ST = M4in:NewSection("T3ST")
-    
+
+    -- Universal client bypass (from alt.lua behavior)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/V.G_Hub_Extras/main/Universal_Client_Bypass"))()
+
     T3ST:NewButton("T3ST", "T3ST", function()
         print("T3ST3ED SUCC3SSFULLY!")
         game.StarterGui:SetCore("SendNotification", {
-            Title = "T3ST3ED SUCC3SSFULLY!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-            })
-        end)
-    T3ST:NewButton("Fly T3ST", "fly", function()
-        loadstring(game:HttpGet('https://pastebin.com/raw/2J4GfniA'))()
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "FLY!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-            })
+            Title    = "T3ST3ED SUCC3SSFULLY!";
+            Text     = "T3rmin4tor, - Exploiting at it's finest";
+            Icon     = "rbxassetid://14682870738";
+            Duration = 5;
+        })
     end)
-    T3ST:NewButton("D4RK D3X", "D4RK D3X", function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua'))()
+
+    T3ST:NewButton("Fly T3ST", "fly", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/2J4GfniA"))()
         game.StarterGui:SetCore("SendNotification", {
-            Title = "CH4NG3 TH3 G4ME!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-            })
+            Title    = "FLY!";
+            Text     = "T3rmin4tor, - Exploiting at it's finest";
+            Icon     = "rbxassetid://14682870738";
+            Duration = 5;
+        })
+    end)
+
+    T3ST:NewButton("D4RK D3X", "D4RK D3X", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua"))()
+        game.StarterGui:SetCore("SendNotification", {
+            Title    = "CH4NG3 TH3 G4ME!";
+            Text     = "T3rmin4tor, - Exploiting at it's finest";
+            Icon     = "rbxassetid://14682870738";
+            Duration = 5;
+        })
     end)
 end
 
+---------------------------------------------------------------------
+-- GUT5 4ND BL4CK P0WD3R (PlaceId: 12334109280)
+---------------------------------------------------------------------
 if game.PlaceId == 12334109280 then
     local M4in = Window:NewTab("M4in")
     local GUT5 = M4in:NewSection("GUT5 4ND BL4CK P0WD3R")
 
-    GUT5:NewButton("3SP", "3SP", function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/sabplay/multiscriptgateway/main/g%26bp.lua'))()
-        game.StarterGui:SetCore("SendNotification", {
-            Title = "S33 TH3M!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-            })
-        end)
+    -- Universal client bypass (from alt.lua behavior)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/V.G_Hub_Extras/main/Universal_Client_Bypass"))()
 
-        GUT5:NewButton("Fly T3ST", "fly", function()
-            loadstring(game:HttpGet('https://pastebin.com/raw/2J4GfniA'))()
-            game.StarterGui:SetCore("SendNotification", {
-            Title = "FLY!"; -- the title (ofc)
-            Text = "T3rmin4tor, - Exploiting at it's finest"; -- what the text says (ofc)
-            Icon = "rbxassetid://14682870738"; -- the image if u want. 
-            Duration = 5; -- how long the notification should in secounds
-               })
+    GUT5:NewButton("3SP", "3SP", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/sabplay/multiscriptgateway/main/g%26bp.lua"))()
+        game.StarterGui:SetCore("SendNotification", {
+            Title    = "S33 TH3M!";
+            Text     = "T3rmin4tor, - Exploiting at it's finest";
+            Icon     = "rbxassetid://14682870738";
+            Duration = 5;
+        })
+    end)
+
+    GUT5:NewButton("Fly T3ST", "fly", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/2J4GfniA"))()
+        game.StarterGui:SetCore("SendNotification", {
+            Title    = "FLY!";
+            Text     = "T3rmin4tor, - Exploiting at it's finest";
+            Icon     = "rbxassetid://14682870738";
+            Duration = 5;
+        })
     end)
 end
